c08760991c3d3209f576b3ef92df3483f373ee3a
emcbrera <emcabrera@estudiante.uniajc.edu.co>
2026-06-15 12:16:18 -0500

    Se crea el componente para la interfaz recordar contraseña

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
+import { RecoverPassword } from './features/auth/pages/recover-password/recover-password';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'login',
     component:Login
+  },
+  {
+    path: 'recover-password',
+    component:RecoverPassword
   }
 ];
